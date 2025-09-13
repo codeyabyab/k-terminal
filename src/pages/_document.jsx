f2b1d918364ff693d81db7a6fc0d063d9e7dd913
codeyabyab <kobyabyab@gmail.com>
2025-09-13 23:00:57 +0800

    13/9/2025 - Updated Document Page to have Improved Layout

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,14 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+
+          <meta name="theme-color" content="#D79921" />
+          <meta name="description" content="K Terminal | Codeyabyab" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
