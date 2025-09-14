a248a61495264a5cd4ea19e510a1e81f2a99a42f
codeyabyab <kobyabyab@gmail.com>
2025-09-14 16:44:57 +0800

    14/9/2025 - Updated Utility Commands to remove Unnecessary Arguments

--- src/utils/bin/utils.js
+++ src/utils/bin/utils.js
@@ -1,27 +1,27 @@
 import packageJson from '../../../package.json';
 import * as bin from './index';
 
-export const help = async (_args) => {
+export const help = async () => {
   const commands = Object.keys(bin).sort().join(', ');
 
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
-export const whoami = async (_args) => {
+export const whoami = async () => {
   return 'guest';
 };
 
-export const date = async (_args) => {
+export const date = async () => {
   return new Date().toString();
 };
 
-export const email = async (_args) => {
+export const email = async () => {
   window.open('mailto:kobyabybab@gmail.com');
 
   return 'Opening mailto:kobyabybab@gmail.com...';
 };
 
-export const repo = async (_args) => {
+export const repo = async () => {
   setTimeout(function () {
     window.open('https://github.com/codeyabyab/k-terminal', '_blank');
   }, 1000);
@@ -30,7 +30,7 @@ export const repo = async (_args) => {
 };
 
 
-export const banner = (_args) => {
+export const banner = () => {
   return `
 ██╗  ██╗    ████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗     
 ██║ ██╔╝    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║     
