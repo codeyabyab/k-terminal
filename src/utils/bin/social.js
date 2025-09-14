c11b2d0788b4da649dfdb84bcd975ddcf6d243eb
codeyabyab <kobyabyab@gmail.com>
2025-09-14 16:46:15 +0800

    14/9/2025 - Updated Social Commands to remove Unnecessary Arguments

--- src/utils/bin/social.js
+++ src/utils/bin/social.js
@@ -1,12 +1,12 @@
 import config from '../../../config.json';
 
-export const github = async (_args) => {
+export const github = async () => {
   window.open(`https://github.com/${config.social.github}/`);
 
   return 'Opening github...';
 };
 
-export const linkedin = async (_args) => {
+export const linkedin = async () => {
   window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
 
   return 'Opening linkedin...';
