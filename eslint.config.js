7c59689ccd92b4b2d5e9221a87dcd7040d123d7f
codeyabyab <kobyabyab@gmail.com>
2025-09-14 17:07:14 +0800

    14/9/2025 - Updated ESLint Config to have Next JS Presets

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'next/core-web-vitals',
     ],
     languageOptions: {
       ecmaVersion: 2020,
